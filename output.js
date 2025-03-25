92e5da704ada3a860cc83daca35a7538262f4222
Bsurpriseua <bsurprise@albany.edu>
2025-03-24 20:06:39 -0400

    Add files via upload

---
+++ output.js
@@ -0,0 +1,15 @@
+function showInnerHTML() {
+	document.getElementById("output1").innerHTML = "Hello from Brayden!";
+}
+
+function useDocumentWrite() {
+	document.write("This replaces the page content! Dont do that please its very incovenient.");
+}
+
+function showAlert() {
+	alert("This sounds the alarm and your computer turns into an active bomb");
+}
+
+function showConsole() {
+	console.log("BOO");
+}
---
+++ variable.js
@@ -0,0 +1,11 @@
+let name = "Crystal";
+let age = 25;
+let isStudent = true;
+
+document.getElementById("demo1").innerHTML = "Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent;
+
+var city = "Albany";
+let state = "NY";
+const country = "USA";
+
+document.getElementById("demo2").innerHTML = "Location: " + city + ", " + state + ", " + country;
